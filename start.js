5299ba419238d981600da917edaef1db8dde45a4
drewlevinson <drewmlevinson@gmail.com>
2026-02-04 14:29:45 -0500

    Update start.js

--- start.js
+++ start.js
@@ -29,11 +29,13 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/www"));
 
-// This is for web server to start listening to port 3000
-app.set("port", 3000);
-var server = app.listen(app.get("port"), function () {
-  console.log("Server listening on port " + server.address().port);
+// Use Render-assigned port in production, fallback to 3000 locally
+const port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
+  console.log("Server listening on port " + port);
 });
+
 
 //-------------------------------------------------------------------
 // Configuration for your APS account
