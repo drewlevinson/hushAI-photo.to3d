665fdec1d5dde823046e1865f776e7f4a73de5b8
drewlevinson <drewmlevinson@gmail.com>
2026-02-04 14:32:27 -0500

    Update start.js

--- start.js
+++ start.js
@@ -30,10 +30,9 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/www"));
 
 // Use Render-assigned port in production, fallback to 3000 locally
-const port = process.env.PORT || 3000;
-
-var server = app.listen(port, function () {
-  console.log("Server listening on port " + port);
+app.set("port", 3000);
+var server = app.listen(app.get("port"), function () {
+  console.log("Server listening on port " + server.address().port);
 });
 
 
